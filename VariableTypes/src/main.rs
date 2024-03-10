d1a106836f7ca438e802d403588ada83ed9b9d1f
Triet <76050658+trietmt9@users.noreply.github.com>
2024-03-10 16:23:18 +0800

    update some data types

--- VariableTypes/src/main.rs
+++ VariableTypes/src/main.rs
@@ -11,4 +11,17 @@ fn main() {
     println!("This is a float data type: {}", myFloat);
     println!("This is a boolean data type: {}", myBoolean);
     println!("This is a character data type: {}", iconChars);
+
+
+    // this is list of some available types
+    let mut int:i8 = 0x01;
+    let mut int16:i16 = 0x0001;
+    let mut int32:i32 = 0x00000001;
+    let mut int64:i64 = 0x00000000FFFFFFFF;
+    let mut uint:u8 = 0xFF;
+    let mut uint16:u16 = 0xFFFF;
+    let mut uint32:u32 = 0xFFFFFFFF;
+    let mut uint64:u64 = 0xFFFFFFFFFFFFFFFF;
+    let mut float:f32 = 0.01;
+    let mut char:char = '\x00';
 }
--- Variables/src/main.rs
+++ Variables/src/main.rs
@@ -2,4 +2,15 @@ fn main() {
     let name = "Me";
     let age = 23; 
     println!("My name is {} and this is my age {}", name, age);
+
+    //mutable variables 
+    let mut var:i32 = 25000;
+    println!("My variable is {}", var);
+    var = 32555; 
+    println!("My changed variable is {}", var);
+
+    // test variables 
+    let mut int:i8 = 0x01;
+    let mut float:f32 = 0.01;
+    let mut char:char = '\x00';
 }
